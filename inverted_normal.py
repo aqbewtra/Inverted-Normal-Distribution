6d07399894296f0c0457e1a6bc1d4a412dba80c0
Avi <aqbewtra@gmail.com>
2021-03-16 18:58:58 -0400

    create inverted_normal.py

---
+++ inverted_normal.py
@@ -0,0 +1,12 @@
+import torch
+from torch.distributions import Distribution
+
+class InvertedNormal(Distribution):
+    def cdf(x):
+        return
+
+    def pdf(x):
+        return
+    
+    def mgf(x):
+        return
