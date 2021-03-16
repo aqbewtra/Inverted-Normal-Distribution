variance fix

variance.py
@@ -7,7 +7,11 @@ def var(sigma):
     return torch.erf(torch.tensor(1/((2**(1/2))*sigma)))+ 1 - 2/(sigma*((2*torch.pi)**(1/2)))
 
 if __name__ == "__main__":
+    '''
     sigma = fsolve(var, .4)
     f = open("variance.txt", "w")
-    f.write(str(sigma))
+    f.write(str(sigma)[1:-1])
     f.close()
+    '''
+    f = open("variance.txt", "r")
+    print(float(f.readline()))